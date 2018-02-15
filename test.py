70299c26aeafc0803affe2620802506ee13502f1
varcher-me <varcher@126.com>
2018-02-16 01:06:37 +0800

    OOP Basically available.

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from Class.SagaFile.SagaFile import SagaFile
 from Class.Handler.GdHandler import GdHandler
-from Class.SagaClass import SagaClass
 
 file = SagaFile('D:/temp/init/', '关于开展2017年信息技术标准化工作检查的通知.gd')
 
