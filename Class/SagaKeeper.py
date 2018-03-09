a6a1851aea1921f0ce8a178c5a055f55a01a6c97
varcher-me <varcher@126.com>
2018-03-10 00:18:00 +0800

    robust for keeper.

--- Class/SagaKeeper.py
+++ Class/SagaKeeper.py
@@ -51,13 +51,14 @@ class SagaKeeper(SagaClass):
                 print("Get jobid = %s" % uuid)
                 file_in_uuid = self.__mysql.get_uuid_fileist(uuid)
                 for file_record in file_in_uuid:
+                    file_seq_no = file_record[0]
+                    file_secure_name = file_record[1]
+                    file = SagaFile(path_init, file_secure_name, uuid, file_seq_no)
                     try:
-                        file_seq_no = file_record[0]
-                        file_secure_name = file_record[1]
-                        file = SagaFile(path_init, file_secure_name, uuid, file_seq_no)
                         file.set_mysql(self.__mysql)
                         file.set_redis(self.__redis)
                         file_ext = file.get_file_ext()
+
                         if ".gd" == file_ext:
                             file.set_handler(self.__gdHandler)
                         if ".doc" == file_ext or ".docx" == file_ext:
@@ -65,16 +66,19 @@ class SagaKeeper(SagaClass):
                         else:
                             except_string = "Unknown ext for file: %s" % (file.get_path_name())
                             raise FileExtUnknownException(except_string)
+
                         file.process()
+
                     except FileExtUnknownException as e:
                         file.update_process_status(CT.CONSTANT_PROCESS_STATUS_FAIL, "INIT", "Unknown ext")
+                        self.mysql().commit()
                         self.get_logger().log(str(e))
+
                     except Exception as e:
                         except_string = "FATAL ERROR: something error, exception is >>>" + str(e) + "<<<."
                         print(except_string)
                         self.__redis.lpush("INIT_QUEUE", uuid)  # resave the uuid as the first job
                         self.get_logger().fatal(except_string)
                         raise e
-                    finally:
-                        self.mysql().commit()
+
             self.heart_beat()
